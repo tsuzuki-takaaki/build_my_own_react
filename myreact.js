cb9912560d17c748f360c3d1edb2a2440d0b528b
alexanderpierce <takaaki.110706@gmail.com>
2023-08-01 14:49:58 +0900

    Fin Step One

--- myreact.js
+++ myreact.js
@@ -1,16 +1,37 @@
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "Hello",
-  },
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => 
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
 }
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      child: []
+    }
+  }
+}
+
+const Didact = {
+  createElement,
+}
+
+const element = Didact.createElement(
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+)
+
 const container = document.getElementById("root")
-
-const node = document.createElement(element.type)
-node["title"] = element.props.title
-const text = document.createTextNode("")
-text["nodeValue"] = element.props.children
-
-node.appendChild(text)
-container.appendChild(node)
+ReactDOM.render(element, container)
